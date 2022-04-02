1870398fd4fcbaca4f4a0cba053860158969a3f8
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2022-04-02 15:43:05 +0200

    Initialize kotlin project

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,2 @@
+group = "nl.avwie"
+version = "1.0-SNAPSHOT"
---
+++ kotlin/dom/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+}
+
+group = "nl.avwie"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
