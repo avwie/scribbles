a788a342e11d73599105d214bc7f0fd1c025ea17
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2022-06-26 16:33:58 +0200

    WIP

--- kotlin/crdt/src/commonMain/kotlin/nl/avwie/crdt/convergent/DistributedMergeable.kt
+++ kotlin/crdt/src/commonMain/kotlin/nl/avwie/crdt/convergent/DistributedMergeable.kt
@@ -1,6 +1,6 @@
 package nl.avwie.crdt.convergent
 
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import nl.avwie.common.UUID
 import nl.avwie.common.uuid
@@ -8,37 +8,49 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 data class DistributedMergeable<T : Mergeable<T>>(
     val states: MutableStateFlow<T>,
-    val updates: MutableSharedFlow<Update<T>>
+    val updates: MutableSharedFlow<Update<T>>,
+    val job: Job
 ) {
     data class Update<T : Mergeable<T>>(val source: UUID, val value: T)
 }
 
-fun <T : Mergeable<T>> T.distribute(
+suspend fun <T : Mergeable<T>> T.distribute(
     updates: MutableSharedFlow<DistributedMergeable.Update<T>>,
     scope: CoroutineScope
 ): DistributedMergeable<T> {
     val source = uuid()
     val states = MutableStateFlow(this)
 
-    states.onEach { newState ->
-        updates.emit(DistributedMergeable.Update(source, newState))
-    }.launchIn(scope)
+    val job = scope.launch {
+        states.onEach { newState ->
+            updates.emit(DistributedMergeable.Update(source, newState))
+        }.launchIn(this)
 
-    updates.onEach { update ->
-        if (update.source == source) {
-            println("Same source: $update")
-            return@onEach
-        }
-        if (update.value == states.value) {
-            println("Same state: $update")
-            return@onEach
-        }
-        val merged = states.value.merge(update.value)
-        println("Merged: $update -> $merged")
-        states.value = merged
-    }.launchIn(scope)
+        updates.onEach { update ->
+            println(update)
+            if (update.source == source) {
+                println("Same source: $update")
+                return@onEach
+            }
+            if (update.value == states.value) {
+                println("Same state: $update")
+                return@onEach
+            }
+            val merged = states.value.merge(update.value)
+            println("Merged: $update -> $merged")
+            states.value = merged
+        }.launchIn(this)
+    }
 
-    return DistributedMergeable(states, updates)
+    // await start
+    scope.launch {
+        while (job.children.any { !it.isActive }) {
+            delay(1)
+        }
+    }.join()
+
+    return DistributedMergeable(states, updates, job)
 }
 
-fun <T : Mergeable<T>> T.distribute(scope: CoroutineScope): DistributedMergeable<T> = distribute(MutableSharedFlow(), scope)
+suspend fun <T : Mergeable<T>> T.distribute(scope: CoroutineScope): DistributedMergeable<T> =
+    distribute(MutableSharedFlow(), scope)
--- kotlin/crdt/src/commonTest/kotlin/nl/avwie/crdt/convergent/DistributedMergeableTests.kt
+++ kotlin/crdt/src/commonTest/kotlin/nl/avwie/crdt/convergent/DistributedMergeableTests.kt
@@ -1,9 +1,10 @@
 package nl.avwie.crdt.convergent
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.cancel
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.Instant
 import nl.avwie.common.uuid
@@ -14,36 +15,33 @@ class DistributedMergeableTests {
 
     @Test
     fun distributedTest() = runTest {
-        selfCancellingScope {
-            val (state, _) = mergeableValueOf("Foo").distribute(this)
-            state.update { mergeableValueOf("Bar") }
-            assertEquals("Bar", state.value.value)
-        }
+        val (state, _, job) = mergeableValueOf("Foo").distribute(this)
+        state.update { mergeableValueOf("Bar") }
+        assertEquals("Bar", state.value.value)
+        job.cancel()
     }
 
     @Test
     fun distributedTest2() = runTest {
-        selfCancellingScope {
-            val (state, updates) = MergeableValue("Bar", Instant.fromEpochMilliseconds(0)).distribute(this)
-
-            val otherSource = uuid()
-            launch {
-                updates.emit(
-                    DistributedMergeable.Update(
-                        otherSource,
-                        MergeableValue("Baz", Instant.fromEpochMilliseconds(1))
-                    )
+        val (state, updates, job) = MergeableValue("Bar", Instant.fromEpochMilliseconds(0)).distribute(this)
+        val otherSource = uuid()
+        launch {
+            updates.emit(
+                DistributedMergeable.Update(
+                    otherSource,
+                    MergeableValue("Baz", Instant.fromEpochMilliseconds(1))
                 )
-                updates.emit(
-                    DistributedMergeable.Update(
-                        otherSource,
-                        MergeableValue("Bat", Instant.fromEpochMilliseconds(2))
-                    )
+            )
+            updates.emit(
+                DistributedMergeable.Update(
+                    otherSource,
+                    MergeableValue("Bat", Instant.fromEpochMilliseconds(2))
                 )
-            }
-            advanceUntilIdle()
-            assertEquals("Bat", state.value.value)
+            )
         }
+        runCurrent()
+        assertEquals("Bat", state.value.value)
+        job.cancel()
     }
 
     suspend inline fun selfCancellingScope(crossinline block: CoroutineScope.() -> Unit) {
