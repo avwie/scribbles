f28b500d157d5732229317faeece5d46599699d1
Arjan van Wieringen <arjan.van.wieringen@gmail.com>
2022-07-02 07:31:30 +0200

    Tweak

--- kotlin/crdt/src/commonTest/kotlin/nl/avwie/crdt/convergent/DistributedMergeableTests.kt
+++ kotlin/crdt/src/commonTest/kotlin/nl/avwie/crdt/convergent/DistributedMergeableTests.kt
@@ -16,63 +16,57 @@ class DistributedMergeableTests {
 
     @Test
     fun singleUser() = runTest {
-        launch {
-            val (distributedMergeable, _) = mergeableValueOf("Foo").distribute(scope = this)
-            distributedMergeable.update { mergeableValueOf("Bar") }
-            assertEquals("Bar", distributedMergeable.value.value)
-            cancel()
-        }.join()
+        val (distributedMergeable, _) = mergeableValueOf("Foo").distribute(scope = this)
+        distributedMergeable.update { mergeableValueOf("Bar") }
+        assertEquals("Bar", distributedMergeable.value.value)
+        currentCoroutineContext().cancelChildren()
     }
 
     @Test
     fun incomingUpdates() = runTest {
-        launch {
-            val updates = MutableSharedFlow<DistributedMessage<MergeableValue<String>>>()
-            val distributedMergeable = MergeableValue("Bar", Instant.fromEpochMilliseconds(0))
-                .distributeIn(updates = updates, scope = this)
+        val updates = MutableSharedFlow<DistributedMessage<MergeableValue<String>>>()
+        val distributedMergeable = MergeableValue("Bar", Instant.fromEpochMilliseconds(0))
+            .distributeIn(updates = updates, scope = this)
 
-            runCurrent() // make sure the handlers are registered
+        runCurrent() // make sure the handlers are registered
 
-            val otherSource = uuid()
-            updates.emit(
-                DistributedMessage(
-                    otherSource,
-                    MergeableValue("Baz", Instant.fromEpochMilliseconds(1))
+        val otherSource = uuid()
+        updates.emit(
+            DistributedMessage(
+                otherSource,
+                MergeableValue("Baz", Instant.fromEpochMilliseconds(1))
             )
-            )
+        )
 
-            updates.emit(
-                DistributedMessage(
-                            otherSource,
-                            MergeableValue("Bat", Instant.fromEpochMilliseconds(2))
-                    )
+        updates.emit(
+            DistributedMessage(
+                otherSource,
+                MergeableValue("Bat", Instant.fromEpochMilliseconds(2))
             )
+        )
 
-            runCurrent() // make sure the events have emitted
-            assertEquals("Bat", distributedMergeable.value.value)
-            cancel()
-        }.join()
+        runCurrent() // make sure the events have emitted
+        assertEquals("Bat", distributedMergeable.value.value)
+        currentCoroutineContext().cancelChildren()
     }
 
     @Test
     fun distributedUpdates() = runTest {
-        launch {
-            val updates = MutableSharedFlow<DistributedMessage<MergeableValue<String>>>()
-            val clientA = MergeableValue("Bar", Instant.fromEpochMilliseconds(0))
-                .distributeIn(updates = updates, scope = this)
-            val clientB = MergeableValue("Baz", Instant.fromEpochMilliseconds(0))
-                .distributeIn(updates = updates, scope = this)
-            runCurrent()
+        val updates = MutableSharedFlow<DistributedMessage<MergeableValue<String>>>()
+        val clientA = MergeableValue("Bar", Instant.fromEpochMilliseconds(0))
+            .distributeIn(updates = updates, scope = this)
+        val clientB = MergeableValue("Baz", Instant.fromEpochMilliseconds(0))
+            .distributeIn(updates = updates, scope = this)
+        runCurrent()
 
-            assertEquals("Baz", clientA.value.value)
-            assertEquals("Baz", clientB.value.value)
+        assertEquals("Baz", clientA.value.value)
+        assertEquals("Baz", clientB.value.value)
 
-            clientA.update { mergeableValueOf("Bat") }
-            runCurrent()
+        clientA.update { mergeableValueOf("Bat") }
+        runCurrent()
 
-            assertEquals("Bat", clientA.value.value)
-            assertEquals("Bat", clientB.value.value)
-            cancel()
-        }.join()
+        assertEquals("Bat", clientA.value.value)
+        assertEquals("Bat", clientB.value.value)
+        currentCoroutineContext().cancelChildren()
     }
 }
