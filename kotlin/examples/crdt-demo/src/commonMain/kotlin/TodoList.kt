fdf2d23ce1a638fdfac2a8a76d21dcb56cc321e7
Arjan van Wieringen <arjan.van.wieringen@bumastemra.nl>
2022-07-06 18:39:16 +0200

    WIP

---
+++ kotlin/examples/crdt-demo/src/commonMain/kotlin/TodoList.kt
@@ -0,0 +1,35 @@
+import nl.avwie.crdt.convergent.*
+
+data class TodoList(
+    private val _name: MergeableValue<String>,
+    private val _items: MergeableMap<String, Boolean>
+) : Mergeable<TodoList>  {
+
+    constructor(name: String) : this(mergeableValueOf(name), mergeableMapOf())
+
+    val name: String by _name
+    val items: Map<String, Boolean> = _items
+
+    fun setName(name: String): TodoList = copy(
+            _name = mergeableValueOf(name)
+    )
+
+    fun addItem(item: String): TodoList = copy(
+        _items = _items.put(item, false)
+    )
+
+    fun removeItem(item: String): TodoList = copy(
+        _items = _items.remove(item)
+    )
+
+    fun finishItem(item: String): TodoList = copy(
+        _items = if (_items.containsKey(item)) _items.put(item, false) else _items
+    )
+
+    override fun merge(other: TodoList): TodoList {
+        return TodoList(
+            _name = _name.merge(other._name),
+            _items = _items.merge(other._items)
+        )
+    }
+}
--- kotlin/examples/crdt-demo/src/jsMain/kotlin/App.kt
+++ kotlin/examples/crdt-demo/src/jsMain/kotlin/App.kt
@@ -1,40 +1,31 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import nl.avwie.common.messagebus.asMessageBus
 import nl.avwie.common.messagebus.deserialize
+import nl.avwie.crdt.convergent.MergeableStateFlow
 import nl.avwie.crdt.convergent.asStateFlow
 import nl.avwie.crdt.convergent.sync
-import nl.avwie.crdt.convergent.mergeableDistantPastValueOf
-import nl.avwie.crdt.convergent.mergeableValueOf
-import org.jetbrains.compose.web.dom.Button
-import org.jetbrains.compose.web.dom.H1
-import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 import org.w3c.dom.BroadcastChannel
-import kotlin.coroutines.EmptyCoroutineContext
 
 fun main() {
-    val scope = CoroutineScope(EmptyCoroutineContext)
-    val channel = BroadcastChannel("updates").asMessageBus(scope)
-    val states = mergeableDistantPastValueOf(0).asStateFlow()
-    states.sync(channel.deserialize(scope), scope)
+    val scope = CoroutineScope(Dispatchers.Default)
+    val channel = BroadcastChannel("updates")
+            .asMessageBus(scope)
+            .deserialize<TodoList>(scope)
+
+    val todoListFlow = TodoList("New list").asStateFlow()
+    todoListFlow.sync(channel, scope)
 
     renderComposable("root") {
-        val counter by states.collectAsState()
-
-        Header("Current value: ${counter.value}")
-        Button("Increase") { states.update { mergeableValueOf(it.value + 1) } }
-        Button("Decrease") { states.update { mergeableValueOf(it.value - 1) } }
+        App(todoListFlow)
     }
 }
 
-@Composable fun Header(title: String) = H1 { Text(title) }
-@Composable fun Button(label: String, onClick: () -> Unit) =
-    Button(
-        attrs = {
-            classes("btn", "btn-primary", "mx-2")
-            onClick { onClick() }
-        }
-    ) { Text(label) }
+@Composable fun App(todoListFlow: MergeableStateFlow<TodoList>) {
+    val todoList = remember { todoListFlow.collectAsState() }
+
+}
